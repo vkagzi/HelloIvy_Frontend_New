700d4295fdf539fd447e7fd4f62dee10a9ad0444
Vishal Saugat <vishalair1@gmail.com>
2026-04-13 18:39:31 +0530

    Removed resume upload

--- app/(saas)/profile/personal/edit/page.tsx
+++ app/(saas)/profile/personal/edit/page.tsx
@@ -234,7 +234,6 @@ const PersonalDetailsForm: React.FC = () => {
   return (
     <div className="flex flex-col gap-4">
       <Instructions />
-      <ResumeUploader onParsed={setParsedResumeData} />
       <Tabs />
       <DynamicForm
         key={JSON.stringify(formDefaults)}
